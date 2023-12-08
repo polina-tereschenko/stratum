5490c8651f7ce98be182298a9ff09c425e90e010
polina-tereschenko <polinatereschenko16@gmail.com>
2023-12-08 18:46:00 +0200

    add help

---
+++ src/help.rs
@@ -0,0 +1,4 @@
+pub fn handle_help() {
+    println!("-p | --pull -> donwload ашду with indicated hash
+              -m | --mount -> none");
+}
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,16 @@
 use std::env;
 mod pull;
 mod mount;
+mod help;
 
 #[tokio::main]
 async fn main() {
     let mut args = env::args().skip(1);
     while let Some(arg) = args.next() {
         match arg.as_str() {
-            "stratum -p" | "stratum -pull" | "-p" | "--pull" => pull::handle_pull(&mut args).await,
-            "stratum -m" | "stratum --mount" | "-m" | "--mount" => mount::handle_mount(),
+            "-p" | "--pull" => pull::handle_pull(&mut args).await,
+            "-m" | "--mount" => mount::handle_mount(),
+            "--help" => help::handle_help(),
             _ => {
                 if arg.starts_with('-') {
                     println!("Unknown argument {}", arg);
